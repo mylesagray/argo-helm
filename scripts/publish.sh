8ad7ca32b83293e7817f8fa2c21d652e3e4f494a
Myles Gray <mg@mylesgray.com>
2021-03-06 19:55:12 +0000

    Temporary override in chart to allow for argo binary changes in v2 breaking dex

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,9 +4,9 @@ set -eux
 SRCROOT="$(cd "$(dirname "$0")/.." && pwd)"
 GIT_PUSH=${GIT_PUSH:-false}
 
-rm -rf $SRCROOT/output && git clone -b gh-pages git@github.com:argoproj/argo-helm.git $SRCROOT/output
+rm -rf $SRCROOT/output && git clone -b gh-pages git@github.com:mylesagray/argo-helm.git $SRCROOT/output
 
-helm repo add argoproj https://argoproj.github.io/argo-helm
+helm repo add argoproj https://mylesagray.github.io/argo-helm
 
 for dir in $(find $SRCROOT/charts -mindepth 1 -maxdepth 1 -type d);
 do
@@ -38,5 +38,5 @@ cd $SRCROOT/output && git status
 
 if [ "$GIT_PUSH" == "true" ]
 then
-    cd $SRCROOT/output && git add . && git commit -m "Publish charts" && git push git@github.com:argoproj/argo-helm.git gh-pages
+    cd $SRCROOT/output && git add . && git commit -m "Publish charts" && git push git@github.com:mylesagray/argo-helm.git gh-pages
 fi
